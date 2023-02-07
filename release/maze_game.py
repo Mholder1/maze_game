a2ac138880e0fc3d56a2d4d258cba36878516e30
Marcus Holder <marcus.holder@devtank.co.uk>
2023-02-07 17:00:05 +0000

    Working on collisions between player and objects.

--- release/maze_game.py
+++ release/maze_game.py
@@ -2,27 +2,29 @@ from pygame.locals import *
 import pygame as pg
 
 class player_t:
-    x = 64
-    y = 64
-    speed = 1
+    def __init__(self, image):
+        self.player_rect = image.get_rect()
+        self.player_rect.x = 32
+        self.player_rect.y = 32
+        self.speed = 1
     
     def move_right(self):
-        self.x = self.x + self.speed
+        self.player_rect.x = self.player_rect.x + self.speed
     
     def move_left(self):
-        self.x = self.x - self.speed
+        self.player_rect.x = self.player_rect.x - self.speed
         
     def move_up(self):
-        self.y = self.y - self.speed
+        self.player_rect.y = self.player_rect.y - self.speed
     
     def move_down(self):
-        self.y = self.y + self.speed
+        self.player_rect.y = self.player_rect.y + self.speed
 
 class maze_t:
     def __init__(self):
         self.m = 10
         self.n = 8
-        self.maze = [1,1,1,1,1,1,1,1,1,1,
+        self.maze = [0,0,1,1,1,1,1,1,1,1,
                      1,0,0,0,0,0,0,0,0,1,
                      1,0,0,0,0,0,0,0,0,1,
                      1,0,1,1,1,1,1,1,0,1,
@@ -30,21 +32,27 @@ class maze_t:
                      1,0,1,0,1,1,1,1,0,1,
                      1,0,0,0,0,0,0,0,0,1,
                      1,1,1,1,1,1,1,1,1,1,]
-        
+                     
     def draw(self, canvas, image_surf):
-        bx = 0
-        by = 0
+        self.bx=0
+        self.by=0
+        self.list_of_x_blocks = []
+        self.list_of_y_blocks = []
+        self.block_rects = []
         for i in range(0, self.m*self.n):
-            if self.maze[ bx + (by*self.m) ] == 1:
-                canvas.blit(image_surf, ( bx * 64, by * 64 ))
+            if self.maze[ self.bx + (self.by*self.m) ] == 1:
+                block = canvas.blit(image_surf, ( self.bx * 64, self.by * 64 ))
+                self.list_of_x_blocks.append(self.bx * 64)
+                self.list_of_y_blocks.append(self.by * 64)
+                self.block_rects.append(block)
             
-            bx = bx + 1
-            if bx > self.m-1:
-                bx = 0
-                by = by + 1
+            self.bx = self.bx + 1
+            if self.bx > self.m-1:
+                self.bx = 0
+                self.by = self.by + 1
                 
 class game_t:
-    window_width  = 750
+    window_width  = 640
     window_height = 500
     player = 0
     
@@ -52,7 +60,6 @@ class game_t:
         self._running = True
         self.canvas = None
         self._image_surf = None
-        self.player = player_t()
         self.bg_color = (234, 210, 168)
         self.maze = maze_t()
     
@@ -62,8 +69,9 @@ class game_t:
                       (self.window_width,self.window_height))
         pg.display.set_caption("Maze Game")
         self._running = True
-        self._image_surf = pg.image.load("../images/adventurer.png").convert()
         self._block_surf = pg.image.load("../images/brick-wall.png").convert()
+        self._image_surf = pg.image.load("../images/player.png").convert()
+        self.player = player_t(self._image_surf)
     
     def on_event(self, event):
         if event.type == QUIT:
@@ -74,15 +82,47 @@ class game_t:
         
     def on_render(self):
         self.canvas.fill(self.bg_color)
-        self.canvas.blit(self._image_surf,
-                         (self.player.x, self.player.y))
+
         self.maze.draw(self.canvas, self._block_surf)
+        self.canvas.blit(self._image_surf,
+                         (self.player.player_rect.x, self.player.player_rect.y))
         pg.display.flip()
     
     def on_cleanup(self):
         pg.quit()
     
+    def check_pos_is_valid(self, x_pos, y_pos, player):
+        if x_pos >= (self.window_width - 64):
+            return False
+        if x_pos <= 0:
+            return False
+        if y_pos >= (self.window_height - 64):
+            return False
+        if y_pos <= 0:
+            return False
+
+        return not len(self.player.player_rect.collidelistall(self.maze.block_rects))
+
+    def check_move_right(self):
+        new_x_pos = self.player.x + self.player.speed
+        new_y_pos = self.player.y
+        return self.check_pos_is_valid(new_x_pos, new_y_pos, self.player) and self.check_pos_is_valid(new_x_pos, new_y_pos - 32, self.player)
     
+    def check_move_left(self):
+        new_x_pos = self.player.x - self.player.speed - 32
+        new_y_pos = self.player.y
+        return self.check_pos_is_valid(new_x_pos - 32, new_y_pos, self.player) and self.check_pos_is_valid(new_x_pos - 32, new_y_pos - 32, self.player)
+
+    def check_move_down(self):
+        new_x_pos = self.player.x
+        new_y_pos = self.player.y + self.player.speed
+        return self.check_pos_is_valid(new_x_pos, new_y_pos, self.player) and self.check_pos_is_valid(new_x_pos - 32, new_y_pos, self.player)
+
+    def check_move_up(self):
+        new_x_pos = self.player.x
+        new_y_pos = self.player.y - self.player.speed - 32
+        return self.check_pos_is_valid(new_x_pos, new_y_pos - 32, self.player) and self.check_pos_is_valid(new_x_pos - 32, new_y_pos - 32, self.player)
+        
     def on_execute(self):
         if self.on_init() == False:
             self._running = False
@@ -92,16 +132,16 @@ class game_t:
             keys = pg.key.get_pressed()
             
             if (keys[K_RIGHT]):
-                self.player.move_right()
+                self.player.player_rect.x += 5
             
             if (keys[K_LEFT]):
-                self.player.move_left()
-            
+                self.player.player_rect.x -= 5
+
             if (keys[K_UP]):
-                self.player.move_up()
-            
+                self.player.player_rect.y -= 5
+
             if (keys[K_DOWN]):
-                self.player.move_down()
+                self.player.player_rect.y += 5
             
             if (keys[K_ESCAPE]):
                 self._running = False
